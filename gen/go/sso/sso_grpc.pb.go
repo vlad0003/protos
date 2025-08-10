85d18a734123fdb7260043a1d14cd8732ae88c6f
Vlad <vlad0303200312@bk.ru>
2025-08-10 10:58:58 +0500

    Update sso_grpc.pb.go

--- gen/go/sso/sso_grpc.pb.go
+++ gen/go/sso/sso_grpc.pb.go
@@ -122,7 +122,6 @@ type UserServiceServer interface {
 
 // UnimplementedUserServiceServer must be embedded to have
 // forward compatible implementations.
-//
 // NOTE: this should be embedded by value instead of pointer to avoid a nil
 // pointer dereference when methods are called.
 type UnimplementedUserServiceServer struct{}
